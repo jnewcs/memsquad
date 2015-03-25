b822e8f275af74fef38c9b133276c8f9e2bbec81
John Newcomb <jnewcs@stanford.edu>
2015-03-25 01:32:34 -0700

    Precompile Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = false
+  config.serve_static_files = true
   config.serve_static_assets = false
+    
   config.assets.precompile =  ['*.js', '*.css', '*.png', '*.jpg'] 
 
   # Compress JavaScripts and CSS.

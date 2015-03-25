47d57f51605b5628a5ba916fec1a8adcf4f6056b
John Newcomb <jnewcs@stanford.edu>
2015-03-25 00:48:28 -0700

    Trying to fix asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,10 @@ module Memsquad
       
     # Allows environment variables to be set before anything else
     config.before_configuration do
-    env_file = File.join(Rails.root, 'config', 'local_env.yml')
-        YAML.load(File.open(env_file)).each do |key, value|
-            ENV[key.to_s] = value
-        end if File.exists?(env_file)
+        env_file = File.join(Rails.root, 'config', 'local_env.yml')
+            YAML.load(File.open(env_file)).each do |key, value|
+                ENV[key.to_s] = value
+            end if File.exists?(env_file)
+        end
     end
-  end
 end
